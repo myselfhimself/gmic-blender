453dd5237a0eea8d17a52a65bb0b6ca35f40dbbe
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-01-27 19:25:58 +0100

    first smallest possible add-on source file

---
+++ __init__.py
@@ -0,0 +1,9 @@
+bl_info = {
+    "name": "Blender G'MIC Add-on",
+    "blender": (2, 80, 0),
+    "category": "Object",
+}
+def register():
+    print("Hello World")
+def unregister():
+    print("Goodbye World")
