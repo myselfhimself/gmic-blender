ffb724218509242e190b3b68568e7f4516d2dc2d
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-01-31 14:40:32 +0100

    paving the way for binary gmic shared libraries auto-loading

--- __init__.py
+++ __init__.py
@@ -3,7 +3,30 @@ bl_info = {
     "blender": (2, 80, 0),
     "category": "Object",
 }
+
+# TODO ensure that globals are tolerated in Blender3d python scripting guidelines..
+__GMIC_LOADED__ = False
+
 def register():
-    print("Hello World")
+    print("Registering " + bl_info["name"])
+    load_gmic_binary_library()
+
 def unregister():
-    print("Goodbye World")
+    print("Unregistring " + bl_info["name"])
+
+def load_gmic_binary_library(self):
+    global __GMIC_LOADED__
+
+    if __GMIC_LOADED__:
+        return
+
+    import os
+    import sys
+    libdir = os.path.join(os.path.dirname(__file__), "gmic-py")
+    if libdir not in sys.path:
+        sys.path.append(libdir)
+    try:
+        import gmic
+        __GMIC_LOADED__ = True
+    except ImportError as err:
+        raise LibraryLoadError("Cannot find fluid engine library: " + libname + "Details:" + sys.exc_info()[0])
