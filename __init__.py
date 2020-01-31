0c643601ed2a21ca406c9f618f6638e555a94c8c
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-01-31 15:52:15 +0100

    fixing addon-make script filename timestamp, adding static gmic-py distribution

--- __init__.py
+++ __init__.py
@@ -9,7 +9,10 @@ __GMIC_LOADED__ = False
 
 def register():
     print("Registering " + bl_info["name"])
-    load_gmic_binary_library()
+    if load_gmic_binary_library():
+        print(dir(gmic))
+    else:
+        print("Failed loading G'MIC binary library :-(")
 
 def unregister():
     print("Unregistring " + bl_info["name"])
@@ -30,3 +33,5 @@ def load_gmic_binary_library():
         __GMIC_LOADED__ = True
     except ImportError as err:
         raise LibraryLoadError("Cannot find fluid engine library: " + libname + "Details:" + sys.exc_info()[0])
+
+    return __GMIC_LOADED__
--- make_addon_zip.bash
+++ make_addon_zip.bash
@@ -1,4 +1,4 @@
-RELEASE_NAME=gmic-blender-addon-$(date +%F_%H_%I_%S)
+RELEASE_NAME=gmic-blender-addon-$(date +%F_%H_%M_%S)
 mkdir $RELEASE_NAME
 cp __init__.py gmic-py/ README.md $RELEASE_NAME -r
 zip downloads/$RELEASE_NAME.zip -r $RELEASE_NAME
