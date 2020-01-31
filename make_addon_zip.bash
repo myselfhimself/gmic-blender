cdd8f7528b81878b8866066603480bf5f6614682
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-01-31 15:10:10 +0100

    new addon-archiving-script

---
+++ make_addon_zip.bash
@@ -0,0 +1,5 @@
+RELEASE_NAME=gmic-blender-addon-$(date +%F_%H_%I_%S)
+mkdir $RELEASE_NAME
+cp __init__.py gmic-py/ README.md $RELEASE_NAME -r
+zip downloads/$RELEASE_NAME.zip -r $RELEASE_NAME
+rm -rf $RELEASE_NAME
