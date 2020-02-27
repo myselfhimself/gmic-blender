87a17805ff511c8dc2b1cb1881d1dc6afef558d4
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-02-27 17:04:13 +0100

    remove gmic-py dir, it should exist only during jobs

--- make_addon_zip.bash
+++ make_addon_zip.bash
@@ -1,8 +1,6 @@
 # Clean old stuff first
-rm -f downloads/*.zip
-rm -f gmic-py/*.whl gmic-py/*.so gmic-py/*.dll
-rm -rf gmic-py/*info*
-rm -rf gmic-py/.libsgmic
+rm -rf downloads/
+rm -rf gmic-py/
 
 # Grab gmic-py .whl from Pypi.org and extract them
 python3 grab_whl_urls.py
@@ -10,6 +8,7 @@ python3 grab_whl_urls.py
 # Zip all of this into the downloads/ directory
 RELEASE_NAME=gmic-blender-addon #-$(date +%F_%H_%M_%S)
 ADDON_PATH=downloads/$RELEASE_NAME.zip
+mkdir -p downloads
 mkdir $RELEASE_NAME
 cp --parents __init__.py gmic-py/ assets/gmic_filters.json README.md $RELEASE_NAME -r
 zip $ADDON_PATH -r $RELEASE_NAME
