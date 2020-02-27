6c07101e2e5c296ced4b727b502aa6b17756b44d
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-02-27 17:34:32 +0100

    selection non-cpython manylinux-only/macosx python 3.5-3.7 gmicpy packages

--- grab_whl_urls.py
+++ grab_whl_urls.py
@@ -3,6 +3,7 @@ import tempfile
 import os
 from distutils.dir_util import copy_tree
 from zipfile import ZipFile
+import re
 
 r = urllib.request.urlopen("https://pypi.org/pypi/gmic/json")
 data = json.loads(r.read())
@@ -10,16 +11,23 @@ releases = list(data["releases"].keys())
 releases.sort()
 latest_release = releases[-1]
 
+def filename_to_skip(filename):
+    """Skip cpython and non-manylinux linux releases"""
+    return re.match(r".*cpython.*|.*-linux-.*", filename) is not None
+
+def python_version_to_keep(filename):
+    return re.match(r".*cp(35|36|37|38).*", filename) is not None
+
 with tempfile.TemporaryDirectory() as dirpath:
     for release in data['releases'][latest_release]:
-        if "cp37" in release["filename"]:
-            # Skip non manylinux linux releases
-            if "-linux-" in release["filename"]:
-                print("skipping " + release["filename"])
+        if python_version_to_keep(release["filename"]):
+            if filename_to_skip(release["filename"]):
                 continue
             print(release["url"])
             datatowrite = urllib.request.urlopen(release["url"]).read()
             filename = release["url"].split("/")[-1]
+            if filename_to_skip(filename):
+                continue
             with open(os.path.join(dirpath, filename), "wb") as f:
                 f.write(datatowrite)
             copy_tree(dirpath, "gmic-py")
@@ -28,4 +36,8 @@ with tempfile.TemporaryDirectory() as dirpath:
             for whl_file in whl_files:
                 with ZipFile(whl_file, 'r') as zipObj:
                     zipObj.extractall('./gmic-py')
+            gmic_py_files = glob.glob("./gmic-py/*.*")
+            for anyfile in gmic_py_files:
+                if filename_to_skip(anyfile):
+                    os.unlink(anyfile)
 
