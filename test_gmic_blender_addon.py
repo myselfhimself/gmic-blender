d4ebe3a1baf74c946f1995e3519676931373bc4e
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-03-06 17:35:49 +0100

    tentative fix huge zipping

--- test_gmic_blender_addon.py
+++ test_gmic_blender_addon.py
@@ -3,4 +3,6 @@ import os
 import blender_addon_tester as BAT
 
 if __name__ == "__main__":
-    BAT.test_blender_addon(addon_path=".", blender_revision="2.80")
+    config = {"blender_load_tests_script": "blender_load_pytest_without_zip_precompression.py"}
+    addon_path = os.path.realpath(os.path.dirname(__file__))
+    BAT.test_blender_addon(addon_path=addon_path, blender_revision="2.80", config=config)
