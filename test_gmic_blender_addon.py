0bce01c5b8281b040104cdbbf1e2209b1922a060
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-03-09 18:22:24 +0100

    tentative use of blender-addon-tester pre-zipped-addon support

--- test_gmic_blender_addon.py
+++ test_gmic_blender_addon.py
@@ -3,6 +3,8 @@ import os
 import blender_addon_tester as BAT
 
 if __name__ == "__main__":
-    config = {"blender_load_tests_script": "blender_load_pytest_without_zip_precompression.py"}
-    addon_path = os.path.realpath(os.path.dirname(__file__))
-    BAT.test_blender_addon(addon_path=addon_path, blender_revision="2.80", config=config)
+    #config = {"blender_load_tests_script": "blender_load_pytest_without_zip_precompression.py"}
+    #addon_path = os.path.realpath(os.path.dirname(__file__))
+    addon_path = "gmic-blender.zip"
+    #BAT.test_blender_addon(addon_path=addon_path, blender_revision="2.80", config=config)
+    BAT.test_blender_addon(addon_path=addon_path, blender_revision="2.80")
