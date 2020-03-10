f432c47bb64d98a8c13f88fbe5e776b00dc2a61a
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-03-10 18:13:44 +0100

    Github Action:: tentative adding coverage as artifact

--- test_gmic_blender_addon.py
+++ test_gmic_blender_addon.py
@@ -3,8 +3,6 @@ import os
 import blender_addon_tester as BAT
 
 if __name__ == "__main__":
-    #config = {"blender_load_tests_script": "blender_load_pytest_without_zip_precompression.py"}
-    #addon_path = os.path.realpath(os.path.dirname(__file__))
+    config = {"coverage": True}
     addon_path = "downloads/gmic_blender.zip"
-    #BAT.test_blender_addon(addon_path=addon_path, blender_revision="2.80", config=config)
-    BAT.test_blender_addon(addon_path=addon_path, blender_revision="2.80")
+    BAT.test_blender_addon(addon_path=addon_path, blender_revision="2.80", config=config)
