fdf0e070ea1c828d1ccd81373b79d17adf7c71f2
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-03-27 14:46:44 +0100

    CI: macos+linux 2.7-2.8nightlies (blender linux .so fix) BLENDER_VERSION injection (2)

--- test_gmic_blender_addon.py
+++ test_gmic_blender_addon.py
@@ -3,6 +3,10 @@ import os
 import blender_addon_tester as BAT
 
 if __name__ == "__main__":
+    blender_revision = os.environ.get("BLENDER_VERSION", False)
+    if not blender_revision:
+        print("Set the BLENDER_VERSION environment variable first!")
+        sys.exit(1)
     config = {"coverage": True}
     addon_path = "downloads/gmic_blender.zip"
-    BAT.test_blender_addon(addon_path=addon_path, blender_revision="2.80", config=config)
+    BAT.test_blender_addon(addon_path=addon_path, blender_revision=blender_revision, config=config)
